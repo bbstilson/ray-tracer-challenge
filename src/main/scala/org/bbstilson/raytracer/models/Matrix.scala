0a1f8e88b876d0e751bef7c3df60274fc8f3c946
Brandon Stilson <bbstilson@fastmail.com>
2019-02-01 08:56:05 -0800

    several matrix ops implemented. few more needed

--- src/main/scala/org/bbstilson/raytracer/models/Matrix.scala
+++ src/main/scala/org/bbstilson/raytracer/models/Matrix.scala
@@ -4,10 +4,44 @@ object VectorToMatrix {
   type Row = Vector[Double]
   type Matrix = Vector[Row]
 
+  object Matrix {
+    def identity(size: Int): Matrix = {
+      (0 to size - 1).map { i =>
+        (0 to size - 1).map { j =>
+          if (i == j) 1d else 0d
+        }.toVector
+      }.toVector
+    }
+  }
+
   implicit class RichMatrix(val m: Matrix) extends AnyVal {
     def apply(m: Matrix) = RichMatrix(m)
 
     def *(other: Matrix): Matrix = RichMatrix.mXm(this.m, other.m)
+
+    // DummyImplicit needed because the Scala/JVM is dumb:
+    // See: https://stackoverflow.com/a/17842474/6147439
+    def *(other: Row)(implicit d: DummyImplicit): Row =
+      RichMatrix.mXr(this.m, other.r)
+
+    def determinant: Double = m(0)(0) * m(1)(1) - m(0)(1) * m(1)(0)
+
+    def submatrix(row: Int, col: Int): Matrix = {
+      RichMatrix
+        .drop1(m, row)
+        .map { RichMatrix.drop1(_, col) }
+    }
+
+    def minor(r: Int, c: Int): Double = m.submatrix(r, c).determinant
+
+    def cofactor(r: Int, c: Int): Double = {
+      val minor = m.minor(r, c)
+      if (r + c % 2 == 0) minor else -minor
+    }
+
+    override def toString: String = {
+      "\n" + m.map { r => r.mkString("\t") }.mkString("\n") + "\n"
+    }
   }
 
   implicit class RichRow(val r: Row) extends AnyVal {
@@ -19,6 +53,19 @@ object VectorToMatrix {
       for (row <- m1) yield
         for (col <- m2.transpose) yield
           RichRow.dotProduct(row, col)
+    }
+
+    def mXr(m: Matrix, r: Row): Row = {
+      for (row <- m) yield RichRow.dotProduct(row, r)
+    }
+
+    def drop1[A](xs: Vector[A], i: Int): Vector[A] = {
+      val size = xs.size
+      (i, size) match {
+        case (0, _) => xs.tail
+        case _ if (i + 1 == size) => xs.init
+        case _ => xs.dropRight(size - i) ++ xs.drop(i + 1)
+      }
     }
   }
 
--- src/test/scala/org/bbstilson/raytracer/models/MatrixSpec.scala
+++ src/test/scala/org/bbstilson/raytracer/models/MatrixSpec.scala
@@ -7,7 +7,7 @@ import org.bbstilson.raytracer.UnitSpec
 class MatrixSpec extends UnitSpec {
   behavior of "Matrix"
 
-  it should "multiply" in {
+  it should "multiply by another matrix" in {
     val m1 = Vector(
       Vector(1d,2d,3d,4d),
       Vector(5d,6d,7d,8d),
@@ -30,5 +30,139 @@ class MatrixSpec extends UnitSpec {
     )
 
     m1 * m2 should be(m3)
+  }
+
+  it should "multiply by a row" in {
+    val m1 = Vector(
+      Vector(1d,2d,3d,4d),
+      Vector(2d,4d,4d,2d),
+      Vector(8d,6d,4d,1d),
+      Vector(0d,0d,0d,1d)
+    )
+    val r1 = Vector(1d,2d,3d,1d)
+    val r2 = Vector(18d,24d,33d,1d)
+
+    m1 * r1 should be(r2)
+  }
+
+  it should "multiply by an identity matrix" in {
+    val m = Vector(
+      Vector(0d,1d,2d,4d),
+      Vector(1d,2d,4d,8d),
+      Vector(2d,4d,8d,16d),
+      Vector(4d,8d,16d,32d)
+    )
+    m * Matrix.identity(4) should be(m)
+  }
+
+  it should "multiply an identity matrix with a row" in {
+    val r = Vector(1d,2d,3d,4d)
+    Matrix.identity(4) * r should be(r)
+  }
+
+  it should "transpose" in {
+    val m1 = Vector(
+      Vector(0,9,3,0),
+      Vector(9,8,0,8),
+      Vector(1,8,5,3),
+      Vector(0,0,5,8)
+    )
+    val m2 = Vector(
+      Vector(0,9,1,0),
+      Vector(9,8,8,0),
+      Vector(3,0,5,5),
+      Vector(0,8,3,8)
+    )
+
+    m1.transpose should be(m2)
+  }
+
+  it should "calculate the determinant of a 2x2 matrix" in {
+    val m1 = Vector(
+      Vector(1d,5d),
+      Vector(-3d,2d)
+    )
+    m1.determinant should be(17)
+  }
+
+  it should "get a submatrix" in {
+    val m1 = Vector(
+      Vector(1d,5d,0d),
+      Vector(-3d,2d,7d),
+      Vector(0d,6d,-3d)
+    )
+    val m2 = Vector(
+      Vector(-3d,2d),
+      Vector(0d,6d)
+    )
+
+    m1.submatrix(0, 2) should be(m2)
+
+    val m3 = Vector(
+      Vector(-6d,1d,1d,6d),
+      Vector(-8d,5d,8d,6d),
+      Vector(-1d,0d,8d,2d),
+      Vector(-7d,1d,-1d,1d)
+    )
+
+    val m4 = Vector(
+      Vector(-6d,1d,6d),
+      Vector(-8d,8d,6d),
+      Vector(-7d,-1d,1d)
+    )
+
+    m3.submatrix(2, 1) should be(m4)
+  }
+
+  it should "calculate the minor of a 3x3 matrix" in {
+    val m1 = Vector(
+      Vector(3d,5d,0d),
+      Vector(2d,-1d,-7d),
+      Vector(6d,-1d,5d)
+    )
+    val m2 = m1.submatrix(1, 0)
+
+    m2.determinant should be(m1.minor(1, 0))
+  }
+
+  it should "calculate a cofactor of a 3x3 matrix" in {
+    val m = Vector(
+      Vector(3d,5d,0d),
+      Vector(2d,-1d,-7d),
+      Vector(6d,-1d,5d)
+    )
+
+    m.minor(0, 0) should be(-12)
+    m.cofactor(0, 0) should be(-12)
+    m.minor(1, 0) should be(25)
+    m.cofactor(1, 0) should be(-25)
+  }
+
+  it should "calculate the determinant of a 3x3 matrix" in {
+    val m = Vector(
+      Vector(1d,2d,6d),
+      Vector(-5d,8d,-4d),
+      Vector(2d,6d,4d)
+    )
+
+    m.cofactor(0,0) should be(56)
+    m.cofactor(0,1) should be(12)
+    m.cofactor(0,2) should be(-46)
+    m.determinant should be(-196)
+  }
+
+  it should "calculate the determinant of a 4x4 matrix" in {
+    val m = Vector(
+      Vector(-2d,-8d, 3d, 5d),
+      Vector(-3d, 1d, 7d, 3d),
+      Vector( 1d, 2d,-9d, 6d),
+      Vector(-6d, 7d, 7d,-9d)
+    )
+
+    m.cofactor(0, 0) should be(690)
+    m.cofactor(0, 1) should be(447)
+    m.cofactor(0, 2) should be(210)
+    m.cofactor(0, 3) should be(51)
+    m.determinant should be(-4071)
   }
 }
