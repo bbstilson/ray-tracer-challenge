123897ced037fd32be5e002c10ea8d3899309360
Brandon Stilson <bbstilson@fastmail.com>
2019-02-06 23:15:56 -0800

    shear matrix + spec

---
+++ src/main/scala/org/bbstilson/raytracer/models/ShearMatrix.scala
@@ -0,0 +1,23 @@
+package org.bbstilson.raytracer.models
+
+import MatrixDouble._
+
+case class ShearMatrix(xy: Int, xz: Int, yx: Int, yz: Int, zx: Int, zy: Int) {
+  private val tMatrix = {
+    val vals = Map(
+      (0,0) -> 1d,
+      (1,1) -> 1d,
+      (2,2) -> 1d,
+      (3,3) -> 1d,
+      (0,1) -> xy.toDouble,
+      (0,2) -> xz.toDouble,
+      (1,0) -> yx.toDouble,
+      (1,2) -> yz.toDouble,
+      (2,0) -> zx.toDouble,
+      (2,1) -> zy.toDouble
+    )
+    mkMatrix(4, 4, (r, c) => vals.getOrElse((r, c), 0d))
+  }
+
+  def *(p: Point): Point = tMatrix * p
+}
