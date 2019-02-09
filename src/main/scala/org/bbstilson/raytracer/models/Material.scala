40fd6e87f76a0ac32ab0bcc52415222cfc3cb1e2
Brandon Stilson <bbstilson@fastmail.com>
2019-02-09 14:07:42 -0800

    lighting algo

--- src/main/scala/org/bbstilson/raytracer/models/Material.scala
+++ src/main/scala/org/bbstilson/raytracer/models/Material.scala
@@ -1,12 +1,58 @@
 package org.bbstilson.raytracer.models
 
+import Math.pow
+
 case class Material(
   color: Color,
   ambient: Double,
   diffuse: Double,
   specular: Double,
   shininess: Double
-)
+) {
+  def light(
+    light: PointLight,
+    point: Point,
+    eye: SceneVector,
+    normal: SceneVector
+  ): Color = {
+    // Combine the surface color with the light's color/intensity.
+    val effectiveColor = color * light.intensity
+
+    // Find the direction to the light source.
+    val lightv = (light.position - point).normalize
+
+    // Compute the ambient contribution.
+    val ambientComputed: Color = effectiveColor * ambient
+
+    // lightDotNormal represents the cosine of the angle between the light
+    // vector and the normal vector. A negative number means the light is on the
+    // other side of the surface.
+
+    val (diffuseComputed: Color, specularComputed: Color) = lightv.dot(normal) match {
+      case lDN if lDN < 0 => (Color.BLACK, Color.BLACK)
+      case lightDotNormal => {
+        // Compute the diffuse contribution.
+        val diffuseComputed = effectiveColor * diffuse * lightDotNormal
+
+        // reflectDotEye represents the cosine of the angle between the reflection
+        // vector and the eye vector. A negative number means the light reflects
+        // away from the eye.
+        val reflectV = SceneVector.reflect(-lightv, normal)
+        val specularComputed = reflectV.dot(eye) match {
+          case rDE if rDE <= 0 => Color.BLACK
+          case reflectDotEye => {
+            // Compute the specular contribution.
+            val factor = pow(reflectDotEye, shininess)
+            light.intensity * specular * factor
+          }
+        }
+      }
+    }
+
+    // Add the three contributions together to get the final shading
+    ambientComputed + diffuseComputed + specularComputed
+  }
+}
 
 object Material {
   def apply(
