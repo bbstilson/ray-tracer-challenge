c566280ed43ede7adaea57f224850dd25846513c
Brandon Stilson <bbstilson@fastmail.com>
2019-02-09 00:16:22 -0800

    printout fun spheres

--- src/main/scala/org/bbstilson/raytracer/models/Scale.scala
+++ src/main/scala/org/bbstilson/raytracer/models/Scale.scala
@@ -6,7 +6,7 @@ import MatrixDouble._
 class Scale(m: Matrix) extends MatrixDouble(m)
 
 object Scale {
-  def apply(x: Int, y: Int, z: Int): Scale = {
+  def apply(x: Double, y: Double, z: Double): Scale = {
     val vals = Vector(x, y, z, 1)
     val m = mkMatrix(4, 4, (r, c) => if (r == c) vals(r) else 0d)
     new Scale(m)
