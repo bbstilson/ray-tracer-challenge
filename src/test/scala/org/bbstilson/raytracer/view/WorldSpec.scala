e03f2b6e0597a604b3a96d79fad8c8b8eb69d0ff
bbstilson <bbstilson@fastmail.com>
2020-02-22 13:39:39 -0800

    abstract world utils

--- src/test/scala/org/bbstilson/raytracer/view/WorldSpec.scala
+++ src/test/scala/org/bbstilson/raytracer/view/WorldSpec.scala
@@ -1,29 +1,12 @@
 package org.bbstilson.raytracer.view
 
+import org.bbstilson.raytracer.TestUtils.WorldUtils
 import org.bbstilson.raytracer.models._
 import org.bbstilson.raytracer.shapes._
 import org.bbstilson.raytracer.transforms._
 import org.bbstilson.raytracer.UnitSpec
 
-class WorldSpec extends UnitSpec {
-
-  val s1 = Sphere(
-    material = Material(
-      color = Color(0.8, 1.0, 0.6),
-      diffuse = 0.7,
-      specular = 0.2
-    )
-  )
-
-  val s2 = Sphere(
-    transform = Scale(0.5, 0.5, 0.5)
-  )
-
-  val light = PointLight(
-    Point(-10, 10, -10),
-    Color(1, 1, 1)
-  )
-  val w = World(List(s1, s2), Some(light))
+class WorldSpec extends UnitSpec with WorldUtils {
 
   it should "create an empty world" in {
     val w = World()
